c7559c1d2af8a4e80b7f470a1983c195e72cf70c
Eric <ericsilva328@gmail.com>
2021-05-11 20:49:24 -0300

    home layout done

--- app/src/main/java/com/example/lolgg/presentation/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/lolgg/presentation/homeFragment/HomeFragment.kt
@@ -5,21 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.example.lolgg.R
+import com.example.lolgg.databinding.FragmentHomeBinding
+import java.lang.reflect.Array
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val items = resources.getStringArray(R.array.regions)
+
+        val adapter = ArrayAdapter(requireContext(), R.layout.item_textview, items)
+
+        binding.spinnerRegions.adapter = adapter
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
